ae4c86de9f94f43801b2771c7e28b5cf77065394
Nick Shorter <36796370+TheFinalJoke@users.noreply.github.com>
2019-03-07 16:26:23 -0600

    Update and rename create-users.py to create-mass-users.py

--- create-mass-users.py
+++ create-mass-users.py
@@ -1,8 +1,10 @@
 import os, crypt
 import sys
 
+#Sets user id
 userID = os.geteuid()
 
+#Checks if User ID is root
 if userID != 0:
     print("Only Root can execute")
     exit(1)
@@ -10,12 +12,14 @@ if len(sys.argv) != 3:
     print("You must have 2 Arguments")
     exit(1)
 
+#Sets arguments
 arg1 = sys.argv[1]
 arg2 = sys.argv[2]
 path = "/home/" + arg1
 
 checking = os.path.isdir(path)
 
+#checks if the directory is there
 if checking == False:
     os.system("mkdir -p " + path)
 
