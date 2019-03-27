fa1db65c26f22667474506016ce702db08d6151f
Nick Shorter <nickshorter@comcast.net>
2019-03-27 07:56:58 -0500

    Update pythonProjectAutomation.py

--- pythonProjectAutomation.py
+++ pythonProjectAutomation.py
@@ -12,74 +12,65 @@ if userID != 0:
 #Checking if has only one argument
 if len(sys.argv) != 2:
     print("This Script only accepts one argument, The username of the node")
-    print("Example: python projectAutomation.py workernodeN")
+    print("Example: python pythonProjectAutomation.py workernodeN")
     print("Exiting Script...")
     exit(1)
 
 #installing Sudo
 print("STATUS Installing Sudo")
-os.system("sleep 3s")
 
-os.system("apt install sudo -y")
+os.system("apt install sudo -y >> /var/log/JohnInstall.log")
 
 #Creating user Master
 print("STATUS: Creating User Master")
-os.system("sleep 3s")
 master = "master:master"
 os.system("useradd -m master")
 os.system("echo " + master + " | chpasswd")
 
 #adding workernodeN to sudoers file
 print("STATUS: Granting " + sys.argv[1] + " and master to sudoers file")
-os.system("sleep 3s")
 os.system('echo "#Giving " ' + sys.argv[1] + ' "and Master sudo privileges" >> /etc/sudoers')
 os.system('echo ' + sys.argv[1] + '" ALL=(ALL:ALL) ALL" >> /etc/sudoers')
 os.system('echo "master ALL=(ALL:ALL) ALL" >> /etc/sudoers')
 
 #exporting PATH to bash_profile
 print("STATUS: EXPORTING PATH")
-os.system("sleep 3s")
 os.system('echo "export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin" >> ~/.bash_profile')
 
 #Installing net-tools
 print("STATUS: Install Net-tools")
-os.system("sleep 3s")
-os.system("apt install net-tools -y")
+os.system("apt install net-tools -y >> /var/log/JohnInstall.log")
 
 #Installing NFS
 print("STATUS: Installing NFS and making NFS Directory")
-os.system("sleep 3s")
-os.system("apt install nfs-common -y")
+os.system("apt install nfs-common -y >> /var/log/JohnInstall.log")
 os.system("mkdir /nfsshare")
 os.system("chmod 777 /nfsshare")
 os.system("echo '192.168.0.20:/nfsshare /nfsshare nfs defaults' >> /etc/fstab")
 
 #Installing JTR And Dependencies
 #Installing Rexgen
-print("STATUS: Installing Rexgen and JTR")
-os.system("sleep 3s")
-os.system("apt update -y")
-os.system("apt install build-essential libssl-dev yasm libgmp-dev libpcap-dev libnss3-dev -y")
-os.system("apt install libkrb5-dev pkg-config libopenmpi-dev openmpi-bin zlib1g-dev libbz2-dev -y")
-os.system("apt install flex cmake bison git -y")
+print("STATUS: Installing Rexgen, JTR, GIT")
+os.system("apt update -y >> /var/log/JohnInstall.log")
+os.system("apt install build-essential libssl-dev yasm libgmp-dev libpcap-dev libnss3-dev -y >> /var/log/JohnInstall.log")
+os.system("apt install libkrb5-dev pkg-config libopenmpi-dev openmpi-bin zlib1g-dev libbz2-dev -y >> /var/log/JohnInstall.log")
+os.system("apt install flex cmake bison git -y >> /var/log/JohnInstall.log")
 
 print("STATUS: Installing Rexgen")
-os.system("sleep 3s")
 os.chdir("/usr/local")
-os.system("git clone https://github.com/teeshop/rexgen.git")
+os.system("git clone https://github.com/teeshop/rexgen.git >> /var/log/JohnInstall.log")
 os.chdir("/usr/local/rexgen")
-os.system("./install.sh")
-os.system("ldconfig")
+os.system("./install.sh >> /var/log/JohnInstall.log")
+os.system("ldconfig >> /var/log/JohnInstall.log")
 
 #Installing JTR
 print("STATUS: Installing JTR")
-os.system("sleep 3s")
 os.chdir("/usr/local")
-os.system("git clone https://github.com/magnumripper/JohnTheRipper.git")
+os.system("git clone https://github.com/magnumripper/JohnTheRipper.git >> /var/log/JohnInstall.log")
 os.system("chmod 777 -R /usr/local/JohnTheRipper")
 os.chdir("/usr/local/JohnTheRipper/src")
-os.system("./configure --enable-mpi")
-os.system("make -s clean && make -sj4")
+os.system("./configure --enable-mpi >> /var/log/JohnInstall.log")
+os.system("make -s clean && make -sj4 >> /var/log/JohnInstall.log")
 os.chdir("/usr/local/JohnTheRipper/run/")
 os.system("./john --test")
 exit(0)
