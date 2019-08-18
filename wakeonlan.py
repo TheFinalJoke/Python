1d082a19804ad5445976d0a0d477923036d7a7cc
Nicholas Shorter <nickshorter@macbook.homelab.int>
2019-08-17 21:12:25 -0500

    Update wakeonlan

--- wakeonlan.py
+++ wakeonlan.py
@@ -1,11 +1,13 @@
 import sys, os
-import wakeonlan
+from wakeonlan import send_magic_packet
 
 def check_host(host):
-    pinganswer = os.system("ping -c 1 " + host + ">> /dev/null")
+    pinganswer = os.system("ping -c 1 > /dev/null " + host)
     if pinganswer != 0:
         return False
     else: 
         return True
 
-check_host("1.1.1.1")
+
+if check_host("1.1.1.1") == True:
+    send_magic_packet('B8:27:EB:E9:BB:9F')
