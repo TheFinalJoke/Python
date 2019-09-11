c2e9ea9bbd334dd5c2fdb4a272cb77eececf8d88
Nick Shorter <nickshorter@fb.com>
2019-09-10 21:57:16 -0500

    modify

--- wakeonlan.py
+++ wakeonlan.py
@@ -1,7 +1,7 @@
 import os
 import dracclient
+client = dracclient.wsman.Client(idrac610.homelab,root,Annkz+319331,port=443)
 
-client = dracclient.client.DRACClient('idrac610.homelab', 'root', 'Annkz+319331', port=443)
 
 def check_host(host):
     pinganswer = os.system("ping -c 1 > /dev/null " + host)
