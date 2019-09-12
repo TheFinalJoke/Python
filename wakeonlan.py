21b121d6b0d41a0789aac7b585904b0465c1fee5
Nick Shorter <nickshorter@fb.com>
2019-09-11 22:07:38 -0500

    fixing wakeonlan

--- wakeonlan.py
+++ wakeonlan.py
@@ -1,18 +1,23 @@
 import os
 from dracclient import client
+#from wakeonlan import send_magic_packet
 
-six_ten = "10.0.14.100"
-client = client.DRACClient(six_ten, 'root', 'Annkz+319331', port=443)
+six_ten = '10.0.14.200'
+client = client.DRACClient(six_ten, 'root', 'Annkz+319331')
 
 
 def check_host(host):
     pinganswer = os.system("ping -c 1 > /dev/null " + host)
     if pinganswer != 0:
+        print(pinganswer)
         return False
     else: 
         return True
 
 
-if check_host("1.1.1.1") == True:
+if check_host("1.1.1.2") == True:
     print(client.get_power_state())
+else:
+    print("host not in pingable")
+
 
