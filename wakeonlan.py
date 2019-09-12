72fbf585fec3964300edafb38a614c37e7f7d3f4
nickshorter <nickshorter@nicksubuntu.homelab>
2019-09-11 23:03:59 -0500

    editing wakeonlan

--- wakeonlan.py
+++ wakeonlan.py
@@ -1,23 +1,27 @@
 import os
+import subprocess
 from dracclient import client
+import logging
 #from wakeonlan import send_magic_packet
-
+logging.basicConfig()
 six_ten = '10.0.14.200'
 client = client.DRACClient(six_ten, 'root', 'Annkz+319331')
 
 
 def check_host(host):
-    pinganswer = os.system("ping -c 1 > /dev/null " + host)
-    if pinganswer != 0:
-        print(pinganswer)
-        return False
-    else: 
-        return True
+    args = ["ping" , "-c 1", host]
+    pinganswer = subprocess.check_output(args)
+    print(pinganswer)
+    #if pinganswer != 0:
+    #    print(pinganswer)
+  #      return False
+  #  else: 
+  #      return True
 
-
-if check_host("1.1.1.2") == True:
-    print(client.get_power_state())
-else:
-    print("host not in pingable")
+check_host('0.0.0.0')
+#if check_host("1.1.1.1") == True:
+  #  print(client.get_power_state())
+#else:
+   # print("host not in pingable")
 
 
