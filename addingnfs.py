2e2dfeaa7807fd790061daecdcb7ac117359dea2
nickshorter <nickshorter@nicksubuntu.homelab>
2019-10-02 18:38:16 -0500

    adding code

--- addingnfs.py
+++ addingnfs.py
@@ -1,1 +1,11 @@
 #adding nfs to all servers
+
+import subprocess
+import parser
+
+checking_distro = subprocess.run(['cat', '/etc/os-release']) 
+print(checking_distro)
+
+
+if (checking_distro == "ubuntu"):
+    
