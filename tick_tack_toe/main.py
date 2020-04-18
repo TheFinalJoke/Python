a4d0460a69656f1d1c962c476d06e25b34fcb1a9
Nicholas Shorter <nickshorter@Nicholass-MacBook-Pro.local>
2020-04-18 00:03:05 -0500

    Finished final commit

--- tick_tack_toe/main.py
+++ tick_tack_toe/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from random import randint
 from enum import Enum
+import pdb
 class board_mapping(Enum):
     A1 = 0
     A2 = 2
@@ -25,10 +26,16 @@ class board():
         self.board = ['','|','','|','',
                       '','|','','|','',
                       '','|','','|','']
-        
+    def check_if_used(self, position):
+            if self.board[position] != '':
+                return True
     def add_to_board(self, position, game_piece):
-        for mapping in board_mapping.name:
-            print(mapping)
+        for placement in board_mapping:
+            if placement.name == position:
+                if not self.check_if_used(placement.value):
+                    self.board[placement.value] = game_piece
+                else:
+                    print('Position has Been used already')
     def del_to_board(self, position):
         pass
     def print_board(self):
@@ -44,8 +51,27 @@ class board():
                print(f' {position} ', end='')
                counter += 1
         
-def check_win():
-    pass   
+def check_win(board_obj):
+    possible_wins = [[0,2,4], [5,7,9], [10,12,14],
+                    [0,7,14], [4,7,10], [0,5,10],
+                    [2,7,12], [4,9,14]]
+    for var in possible_wins:
+        checking = []
+        for index in var:
+            if board_obj.board[index] == 'x':
+                checking.append(board_obj.board[index])
+            elif board_obj.board[index] == 'o':
+                checking.append(board_obj.board[index])
+            else:
+                continue
+        if len(checking) < 3:
+            continue 
+        else:
+            if len(set(checking)) == 1:
+                return True, checking[0]
+            else:
+                continue
+    return False, ''
 def define_players():
     while True:
         try:
@@ -73,34 +99,44 @@ def run():
         player1.turn = True
     else: 
         player2.turn = True
-
     counter = 0 
-    while True:
+    while counter != 9:
         play_board.print_board()
         if player1.turn == True:
             try: 
                 position = input('Player1 turn, Choose Square -> ')
-                if position not in [var for var in board_mapping.name]:
+                if position not in [board_placing.name for board_placing in board_mapping]:
                     raise Exception('Not a valid Square')
             except Exception as e: 
                 print(e)
                 continue
             play_board.add_to_board(position, player1.value)
-            check_win()
+            check_win_var = check_win(play_board)
+            if check_win_var[0]:
+                if check_win_var[1] == player1.value:
+                    play_board.print_board()
+                    return 'Player1 Won'
             player1.turn = False 
             player2.turn = True
+            counter += 1
         else: 
             try: 
                 position = input('Player2 turn, Choose Square -> ')
-                if position not in [value for value in board_mapping.name]:
+                if position not in [board_placing.name for board_placing in board_mapping]:
                     raise Exception('Not a valid Square')
             except Exception as e: 
                 print(e)
                 continue
             play_board.add_to_board(position, player2.value)
-            check_win() 
+            check_win_var = check_win(play_board)
+            if check_win_var[0]:
+                if check_win_var[1] == player2.value:
+                    play_board.print_board()
+                    return 'Player 2 won'
             player2.turn = False 
             player1.turn = True
-    
+            counter += 1
+    print('You ran out of spaces.. Draw')
 if __name__ == '__main__':
-    run()
+    print(run())
+    
